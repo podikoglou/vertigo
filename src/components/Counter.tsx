d60ed0a7064b1df0f971da600fe2f781efcdac87
Alex <alex.podikoglou@gmail.com>
2024-06-22 19:02:12 +0300

    style: format

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,20 +3,20 @@ import { counterAtom } from "../stores/counter";
 import { Button } from "./ui/button";
 
 export default function () {
-  const [counter, setCounter] = useAtom(counterAtom);
-  return (
-    <div>
-      <p>We're at {counter}</p>
+	const [counter, setCounter] = useAtom(counterAtom);
+	return (
+		<div>
+			<p>We're at {counter}</p>
 
-      <div className="flex flex-row gap-1">
-        <Button type="button" onClick={() => setCounter(counter + 1)}>
-          Increment
-        </Button>
+			<div className="flex flex-row gap-1">
+				<Button type="button" onClick={() => setCounter(counter + 1)}>
+					Increment
+				</Button>
 
-        <Button type="button" onClick={() => setCounter(counter - 1)}>
-          Decrement
-        </Button>
-      </div>
-    </div>
-  );
+				<Button type="button" onClick={() => setCounter(counter - 1)}>
+					Decrement
+				</Button>
+			</div>
+		</div>
+	);
 }
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -2,15 +2,15 @@ import Counter from "@/components/Counter";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
-  component: Page,
+	component: Page,
 });
 
 function Page() {
-  return (
-    <div className="container">
-      <h1 className="text-4xl">Hey.</h1>
+	return (
+		<div className="container">
+			<h1 className="text-4xl">Hey.</h1>
 
-      <Counter />
-    </div>
-  );
+			<Counter />
+		</div>
+	);
 }
